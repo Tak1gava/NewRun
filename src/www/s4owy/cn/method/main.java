48842fe870cb1aa00ac06fe7e7cf87a15d1b5e95
Nanzh <1299947653@qq.com>
2025-11-04 22:32:14 +0800

    update

---
+++ src/www/s4owy/cn/method/main.java
@@ -0,0 +1,6 @@
+package www.s4owy.cn.method;
+
+public class main {
+    public static void main(String[] args) {
+    }
+}
--- src/www/s4owy/cn/method/method1.java
+++ src/www/s4owy/cn/method/method1.java
@@ -5,5 +5,10 @@ package www.s4owy.cn.method;
 
 //定义一个方法，该方法能够找出两个小数中的较小值并返回。在主方法中调用方法进行测试。
 
+import java.util.Scanner;
+
 public class method1 {
+    public static void ifSmall(){
+
+    }
 }
